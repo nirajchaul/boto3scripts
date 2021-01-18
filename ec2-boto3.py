4a93213e7465a0aae971de70a234fbe78318ddfd
niraj-chaul <niraj.chaul@gmail.com>
2021-01-17 20:12:28 -0600

    Boto3 script for ec2 instance in US-east-1

---
+++ ec2-boto3.py
@@ -0,0 +1,15 @@
+import boto3
+
+ec2 = boto3.resource('ec2')
+##ec2 = boto3.resource('ec2', region_name='us-west-1')
+##change imageid and keyname as they are region specific
+
+instances = ec2.create_instances(
+    ImageId='ami-0be2609ba883822ec',
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro',
+    KeyName='DevOpsAutomationKey'
+)
+
+ 

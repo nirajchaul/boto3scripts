bfa12e961ef8abbed4a6e26682f59629c1722a2b
niraj-chaul <niraj.chaul@gmail.com>
2021-01-17 22:19:32 -0600

    Revert "Comment added second time"
    This reverts commit 9fb3089e15078196436acf5ff6496b1eecf38501.

--- ec2-boto3.py
+++ ec2-boto3.py
@@ -3,8 +3,6 @@ import boto3
 ec2 = boto3.resource('ec2')
 ##ec2 = boto3.resource('ec2', region_name='us-west-1')
 ##change imageid and keyname as they are region specific
-##comment added. Here I am.
-
 
 instances = ec2.create_instances(
     ImageId='ami-0be2609ba883822ec',

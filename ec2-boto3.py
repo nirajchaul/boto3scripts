299a8d99a13974212135c5da117c2daf30902ceb
niraj-chaul <niraj.chaul@gmail.com>
2021-01-17 23:39:21 -0600

    maxcout to 2 increased

--- ec2-boto3.py
+++ ec2-boto3.py
@@ -7,7 +7,7 @@ ec2 = boto3.resource('ec2')
 instances = ec2.create_instances(
     ImageId='ami-0be2609ba883822ec',
     MinCount=1,
-    MaxCount=1,
+    MaxCount=2,
     InstanceType='t2.micro',
     KeyName='DevOpsAutomationKey'
 )
